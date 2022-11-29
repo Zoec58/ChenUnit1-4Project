c95d08e13bf92a63ccfbdf98060ec3c14eba72e4
Rohit <rohitc@nycstudents.net>
2022-11-29 09:13:36 -0500

    Initial commit

---
+++ src/Dungeon.java
@@ -0,0 +1,14 @@
+public class Dungeon {
+    //variables
+    String name;
+    int atk;
+    int def;
+    int hp;
+
+    public Dungeon(String name) {
+        this.name = name;
+        atk = 3;
+        def = 0;
+        hp = 10;
+    }
+}
