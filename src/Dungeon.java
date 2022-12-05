71b08564a96dc3dd205385df082a3fff23543bc4
Zoe Chen <zoec58@nycstudents.net>
2022-12-05 04:08:28 -0500

    two commands added, unfinished.

--- src/Dungeon.java
+++ src/Dungeon.java
@@ -1,6 +1,7 @@
 public class Dungeon {
     //variables
     String name;
+    String warriorClass;
     int atk;
     int def;
     int hp;
@@ -11,4 +12,13 @@ public class Dungeon {
         def = 0;
         hp = 10;
     }
+
+    public void choosingClass(String chosenClass) {
+
+    }
+
+    public void greeting(){
+        System.out.println("Welcome to [GENERIC MEDIEVAL GAME TITLE]!! This is a text-based game in which you will progress through a dungeon and fight monsters!!")
+    }
+
 }
